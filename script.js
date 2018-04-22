12bb8f359939ed74185ca76212583fab20572d87
r4pt0s <r4pt0s@protonmail.com>
2018-04-22 09:09:10 +0200

    everything is ES5/ES6 Syntax now. Code reviewed

--- script.js
+++ script.js
@@ -1,60 +1,79 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
-var bunchOfLi= document.querySelectorAll("li");
+let button = document.getElementById("enter");		//get the 'enter' button by ID
+let input = document.getElementById("userinput");	// get the input text field by ID
+let ul = document.querySelector("ul");				// use of new ES5 Syntax to get HTML DOM ELEMENT
+let bunchOfLi= document.querySelectorAll("li");		// use of new ES5 Syntax to get a Nodelist 
 
-function inputLength() {
+const inputLength = () => {
+	//returns the count of characters from the input text field
 	return input.value.length;
 }
 
-function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	li.classList.add("list-group-item", "d-flex", "justify-content-between");
-	li.appendChild(addDeleteButton(li));
+// create to new todo task
+const createListElement = () => {
+	let li = document.createElement("li");	//create the List element
+	let span = document.createElement("span"); // create a span for text, otherwise the button text get crossed too
 
+	span.appendChild(document.createTextNode(input.value)); // create the text from input field and append it to 'safty-span'
+	li.classList.add("list-group-item", "d-flex", "justify-content-between"); // do some bootstrap magic
 
-	ul.appendChild(li);
-	input.value = "";
+	li.appendChild(span)	//append the span which holds the text
+	  .parentElement		//move up to the li again. Otherwise the button is nested inside the span too
+	  .appendChild(addDeleteButton(li));	// append the delete Button to
 
+	ul.appendChild(li);	// append the new LI element which holds the span and the delete button to UL element
+	input.value = "";	// delete everthing from input text field
 }
 
-function addListAfterClick() {
+//handle click on 'enter'-Button
+const addListAfterClick = () => {
+	//checks if the input isn't empty
 	if (inputLength() > 0) {
 		createListElement();
 	}
 }
 
-function addListAfterKeypress(event) {
+//handle keypress on KEY: ENTER(keycode:13)
+const addListAfterKeypress =(event) => {
+	//checks if the input isn't empty and 'Enter' was pressed
 	if (inputLength() > 0 && event.keyCode === 13) {
 		createListElement();
 	}
 }
 
-button.addEventListener("click", addListAfterClick);
+const workWithLi = (event) => {
+	const childTarget= event.target;	//contains the target which one was clicked
+										// follows the path from existing ul
+	
+	//if the SPAN element within the ul was clicked
+	if(childTarget.nodeName === 'SPAN'){
+		childTarget.classList.toggle('done');
+	}
 
-input.addEventListener("keypress", addListAfterKeypress);
+	//if the BUTTON element within the ul->li was clicked
+	if(childTarget.nodeName === 'BUTTON'){
+		childTarget			//PATH starts at Button
+			.parentElement	//move up to parent LI
+			.parentElement	//move up to parent UL
+			.removeChild(childTarget.parentElement); //remove the Child of UL which is the parent of the button
+	}												 
+}
 
-bunchOfLi.forEach(function(li, i){
-		li.addEventListener("click", function(e){
-			li.classList.toggle('done');
-		});
-		li.appendChild(addDeleteButton(li));
-
-	});
-
-function addDeleteButton(li){
-
-		var delBtn= document.createElement("button");
-		delBtn.appendChild(document.createTextNode("Delete"));
-		delBtn.classList.add("btn","btn-danger");
-
-		delBtn.addEventListener("click", function(e){
-
-			this.parentElement.parentElement.removeChild(li);
-			//or
-            //e.target.parentElement.parentElement.removeChild(li);
-		});
+const addDeleteButton= (li) =>{
+		var delBtn= document.createElement("button");			//create the Element
+		delBtn.appendChild(document.createTextNode("Delete"));	//create the text node and then apppend it to the elemt
+		delBtn.classList.add("btn","btn-danger");				// does Bootstrap magic with classes
 
 		return delBtn;
 }
+
+//adding Delete Buttons for existing elements
+bunchOfLi.forEach((li, i) => {
+		li.appendChild(addDeleteButton(li));
+});
+
+//add Event Listener to the enter button
+button.addEventListener("click", addListAfterClick);
+//add Event Listener to Keyboard. Only executed if something is typed in the input field and the curser is still active there
+input.addEventListener("keypress", addListAfterKeypress);
+//add Event Listener to UL element which holds all the tasks
+ul.addEventListener("click", workWithLi);
